0fbf0bf3728ca6ea922530bf8958e25b7afc2ac7
Mikhail Sakhnyuk <its545@yandex.ru>
2019-07-03 22:48:06 +0700

    Clear code

--- src/js/app.js
+++ src/js/app.js
@@ -5,39 +5,13 @@ $(function () {
       server: $('#server-address').val(),
       name: $('#name').val(),
       exten: $('#exten').val(),
-      password: $('#password').val(),
-      audioId: 'remote-stream-audio',
-      localVideoId: 'local-stream-video',
-      remoteVideoId: 'remote-stream-video'
+      password: $('#password').val()
     });
-  });
-
-  $('#login-btn').click(function () {
-    webrtcPhone.login();
-  });
-
-  $('#logout-btn').click(function () {
-    webrtcPhone.logout();
   });
 
   $('#call-btn').click(function () {
     var to = $('#call-to').val();
     webrtcPhone.call(to);
   });
-
-  $('#call-video-btn').click(function () {
-    var to = $('#call-video-to').val();
-    webrtcPhone.callAudioVideo(to);
-  });
-
-  $('#answer-btn').click(function () {
-    webrtcPhone.answer();
-  });
-
-  $('#hangup-btn').click(function () {
-    webrtcPhone.hangup();
-  });
-
-  // $('#init-btn').click();
   
 });
